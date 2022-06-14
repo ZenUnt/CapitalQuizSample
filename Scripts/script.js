a2ccb055d4c909eacfee5caa5c487c5a0d5f357c
Zen <br343nh@yahoo.co.jp>
2022-06-14 22:57:20 +0900

    footerの共通化。

--- Scripts/script.js
+++ Scripts/script.js
@@ -2,6 +2,7 @@ $(function() {
     'use strict';
 
     $('#header').load('./header.html');
+    $('#footer').load('./footer.html');
 
     let date1, date_gmt;
     let kigen = 3000; //cookieの期限（今回は3000日）
